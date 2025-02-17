6da1fa9a537c6cd492c073866d96cc13132a0a55
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-17 20:50:57 +0200

    Implement XML stock processing and database update

--- src/main/java/database/DatabaseManager.java
+++ src/main/java/database/DatabaseManager.java
@@ -1,5 +1,7 @@
 package database;
 
+import utils.ConfigLoader;
+
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,26 +9,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseManager {
-    private static String url;
-    private static String username;
-    private static String password;
-
-    static {
-        try(InputStream input = DatabaseManager.class.getClassLoader().getResourceAsStream("application.properties")){
-            if(input == null){
-                System.out.println("(Sorry, unable to find application.properties");
-            }
-            Properties properties = new Properties();
-            properties.load(input);
-
-            url = properties.getProperty("db.url");
-            username = properties.getProperty("db.username");
-            password = properties.getProperty("db.password");
-
-        } catch (Exception exception){
-            exception.printStackTrace();
-        }
-    }
+    private static String url = ConfigLoader.getProperty("db.url");
+    private static String username = ConfigLoader.getProperty("db.username");
+    private static String password  = ConfigLoader.getProperty("db.password");
 
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(url, username, password);
---
+++ src/main/java/utils/ConfigLoader.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigLoader {
+
+    private static final Properties properties = new Properties();
+
+    static {
+        try (FileInputStream input = new FileInputStream("src/main/resources/application.properties")) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
